5c6ed395bedd9fa5b92b9d3c223bc1002c9e06f7
Josh Allen <largozor@josha.au>
2025-11-09 17:46:54 +1100

    GRPG-player-setup
    Initial player setup and basic movement

---
+++ scripts/player/player.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+@export var move_speed: float = 100
+@export var hp: int = 10
+
+@onready var sprite = $AnimatedSprite2D
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta) -> void:
+	
+	var move_vector: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	velocity = move_vector * move_speed
+	
+	if velocity.x > 0:
+		sprite.play("walk_right")
+	elif velocity.x < 0:
+		sprite.play("walk_left")
+	elif velocity.y > 0:
+		sprite.play("walk_down")
+	elif velocity.y < 0:
+		sprite.play("walk_up")
+	if velocity == Vector2(0,0):
+		sprite.stop()
+	
+	move_and_slide()
