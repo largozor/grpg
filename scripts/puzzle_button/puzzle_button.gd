c896d8591d57e246cca9ed7d147db23450f7c7b2
Josh Allen <largozor@josha.au>
2025-11-28 12:15:31 +1100

    GRPG-added-ice-block-puzzle
    Added single use button and ice block puzzle

--- scripts/puzzle_button/puzzle_button.gd
+++ scripts/puzzle_button/puzzle_button.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 @onready var sprite = $AnimatedSprite2D
 
+@export var is_single_use: bool
+
 var bodies_on_top: int = 0
 
 signal pressed
@@ -20,6 +22,6 @@ func _on_body_exited(body: Node2D) -> void:
 	bodies_on_top -= 1
 	print("bodies on top: " + str(bodies_on_top))
 	if body.is_in_group("pushable") or body is Player:
-		if bodies_on_top == 0:
+		if bodies_on_top == 0 and not is_single_use:
 			sprite.play("unpressed")
 			emit_signal("unpressed")
