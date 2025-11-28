09ca8dd664ffeffaf977b6139431c95888fc0669
Josh Allen <largozor@josha.au>
2025-11-29 09:49:57 +1100

    GRPG-basic-dialog-box
    Created a basic dialog box

--- scripts/npc/npc.gd
+++ scripts/npc/npc.gd
@@ -1,5 +1,12 @@
 extends StaticBody2D
 
+@onready var dialog_box = $CanvasLayer
+@onready var ui_label = $CanvasLayer/TextureRect/Label
+
 func _process(delta):
 	if Input.is_action_just_pressed("interact"):
-		print("Hello there!")
+		#print("Hello there!")
+		if dialog_box.visible == true:
+			dialog_box.visible = false;
+		else:
+			dialog_box.visible = true
